5d4d40b238697dc764c8283a2b591c3113cc0167
walterbaya <walterbaya@yahoo.com>
2020-02-18 16:10:37 -0300

    Se agrega ejercicio 4

--- Laboratorios Algoritmos I/Laboratorio 07-20200218/template_alumnos/ejercicios.cpp
+++ Laboratorios Algoritmos I/Laboratorio 07-20200218/template_alumnos/ejercicios.cpp
@@ -31,9 +31,27 @@ vector<vector<int> > multiplicar(vector<vector<int> > m1, vector<vector<int> > m
     return res;
 }
 
+int auxPromedioVecinos(vector<vector<int> > m, int i, int j) {
+    int suma = m[i][j];
+    int cantidad = 0;
+    for (int k = j - 1; k <= j + 1; k++) {
+        for (int l = i - 1; l <= i + 1; l++) {
+            if (k >= 0 && k < m.size() && l >= 0 && l < m[0].size()) {
+                suma = m[k][l] + suma;
+                cantidad++;
+            }
+        }
+    }
+    return suma / cantidad;
+}
+
 vector<vector<int> > promediar(vector<vector<int> > m) {
-    //COMPLETAR
-    vector<vector<int> > res;
+    vector<vector<int> > res(m.size(), vector<int>(m[0].size()));
+    for (int i = 0; i < m.size(); i++) {
+        for (int j = 0; j < m[0].size(); j++) {
+            res[i][j] = auxPromedioVecinos(m, i, j);
+        }
+    }
     return res;
 }
 
