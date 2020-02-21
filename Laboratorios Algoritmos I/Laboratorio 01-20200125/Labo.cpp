41b13aa823aa4dbe468edf88c6662f9e20ebc00d
walterbaya <walterbaya@yahoo.com>
2020-02-21 10:25:26 -0300

    se suben ejercicio 6 y 7

--- Laboratorios Algoritmos I/Laboratorio 01-20200125/Labo.cpp
+++ Laboratorios Algoritmos I/Laboratorio 01-20200125/Labo.cpp
@@ -1,4 +1,6 @@
-# include <iostream >
+# include <iostream>
+
+using namespace std;
 /*Ejercicio 2
  int f(int x, int y) {
     return x + y;
@@ -28,18 +30,18 @@ bool esPrimo(int n) {
 }
 //Ejercicio 5 Recursivo
 
-int fibonacciRecursivo(int n) {
+int fibonacciR(int n) {
     if (n == 0) {
         return 0;
     } else if (n == 1) {
         return 1;
     } else {
-        return fibonacciRecursivo(n - 1) + fibonacciRecursivo(n - 2);
+        return fibonacciR(n - 1) + fibonacciR(n - 2);
     }
 }
 
 //Ejercicio 5 iterativo con While
-int fibonacciIterativo(int n) {
+int fibonacciW(int n) {
     int nMenos2 = 0;
     int nMenos1 = 1;
     int res = 0;
@@ -54,7 +56,7 @@ int fibonacciIterativo(int n) {
 }
 
 //Ejercicio 5 iterativo con for
-int fibonacciIterativoConFor(int n) {
+int fibonacciF(int n) {
     int nMenos2 = 0;
     int nMenos1 = 1;
     int res = 0;
@@ -67,20 +69,91 @@ int fibonacciIterativoConFor(int n) {
 }
 
 //Ejercicio 6 Recursivo
-int sumaImpares(int n){
-    if(n == 1){
+int sumaImparesR(int n) {
+    if (n == 1) {
         return 0;
     }
-    return (n % 2 != 0) ?  n + sumaImpares(n-1) : sumaImpares(n-1);
+    return (n % 2 != 0) ? n + sumaImparesR(n - 1) : sumaImparesR(n - 1);
+}
+
+//Ejercicio 6 iterativo con while
+int sumaImparesW(int n) {
+    int i = 0;
+    int suma = 0;
+    while (i < n) {
+        if (i % 2 != 0) {
+            suma = suma + i;
+        }
+    }
+    return suma;
+}
+
+//Ejercicio 6 iterativo con for
+int sumaImparesF(int n) {
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 != 0) {
+            suma = suma + i;
+        }
+    }
+    return suma;
+}
+
+//Ejercicio 7 recursivo
+int divisoresHasta(int n, int k) {
+    if (k == 1) {
+        return 1;
+    } else if (n % k == 0) {
+        return k + divisoresHasta(n, k - 1);
+    } else {
+        return divisoresHasta(n, k - 1);
+    }
+}
+
+//Ejercicio 7 iterativo con while
+int sumaDivisoresW(int n) {
+    int i = 0;
+    int suma = 0;
+    while (i < n) {
+        if (n % i == 0) {
+            suma = suma + i;
+        }
+    }
+    return suma;
 }
 
 
+//Ejercicio 7 iterativo con for
+int sumaDivisoresF(int n) {
+    int suma = 0;
+    for (int i = 0; i < n; i++) {
+        if (n % i == 0) {
+            suma = suma + i;
+        }
+    }
+    return suma;
+}
+
+//Ejercicio 8 recursivo
+
+
+//Ejercicio 8 iterativo con while
+
+
+//Ejercicio 8 iterativo con for
+
+
 int main() {
-    std::cout << "El resultado es: " << f(2, 3) << std::endl;
-    std::cout << "Es primo?: " << esPrimo(17) << std::endl;
-    std::cout << "Numero fibonacciRecursivo: " << fibonacciRecursivo(6) << std::endl;
-    std::cout << "Numero fibonacciIterativo: " << fibonacciIterativo(6) << std::endl;
-    std::cout << "Numero fibonacciIterativo con For: " << fibonacciIterativoConFor(6) << std::endl;
-    std::cout<< "Suma impares Recursivo: " << sumaImpares(7)<< std::endl;
+    cout << "El resultado es: " << f(2, 3) << endl;
+    cout << "Es primo?: " << esPrimo(17) << endl;
+    cout << "Numero fibonacciRecursivo: " << fibonacciR(6) << endl;
+    cout << "Numero fibonacciIterativo: " << fibonacciW(6) << endl;
+    cout << "Numero fibonacciIterativo con For: " << fibonacciF(6) << endl;
+    cout << "Suma impares Recursivo: " << sumaImparesR(7) << endl;
+    cout << "Suma impares Recursivo: " << sumaImparesW(7) << endl;
+    cout << "Suma impares Recursivo: " << sumaImparesF(7) << endl;
+    cout << "suma divisores Recursivo: " << sumaDivisoresR(9) << endl;
+    //cout << "suma divisores Recursivo: " << sumaDivisoresW(9) << endl;
+    //cout << "suma divisores Recursivo: " << sumaDivisoresF(9) << endl;
     return 0;
 }
