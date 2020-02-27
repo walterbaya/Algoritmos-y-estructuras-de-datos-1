d1995178dd17da497ca7d78dc4f842ef85266695
walterbaya <walterbaya@yahoo.com>
2020-02-27 18:52:23 -0300

    se sube ejercicio 5

--- Laboratorios Algoritmos I/Laboratorio 05-20200125/template-alumnos/ejercicios.cpp
+++ Laboratorios Algoritmos I/Laboratorio 05-20200125/template-alumnos/ejercicios.cpp
@@ -13,6 +13,8 @@ bool existePico(vector<int> v) {
     }
     return res;
 }
+//Si habria que modificar el codigo reemplazando el res por true al inicio y que cuando encuentre un pico se haga falso
+//en ese caso se cumple el invariante.
 
 // Ejercicio 2
 int mcd(int m, int n) {
@@ -31,8 +33,32 @@ void ordenar1(vector<int> &v) {
 
 // Ejercicio 5
 void ordenar2(vector<int> &v) {
-    return;
+    int ceros = 0;
+    int unos = 0;
+    int dos = 0;
+    for (int i = 0; i < v.size(); i++) {
+        if (v[i] == 0) {
+            ceros++;
+        } else if (v[i] == 1) {
+            unos++;
+        } else {
+            dos++;
+        }
+    }
+    for (int j = 0; j < ceros; j++) {
+        v[j] = 0;
+    }
+    for (int k = ceros; k < unos + ceros; k++) {
+        v[k] = 1;
+    }
+    for (int l = unos + ceros; l < dos + unos + ceros; l++) {
+        v[l] = 2;
+    }
 }
+
+//la idea de este ejercicio es hacer algo muy parecido a una cosa llamada bucket sort que se ve en algoritmos II
+//esta forma toma O(n) es mas eficiente que la manera ingenua de hacerlo, de todas maneras el objetivo del ejercicio
+//no era pensar en complejidades
 
 // Ejercicio 6
 tuple<int, int> division(int n, int d) {
