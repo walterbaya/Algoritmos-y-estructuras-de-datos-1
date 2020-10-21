c6f344732cd4d672ac0482904e7ba2ad56949966
walterbaya <walterbaya@yahoo.com>
2020-10-21 00:03:12 -0300

    se sube item 3.1.1 y 3.1.2

--- Laboratorios Algoritmos I/Laboratorio 06-20200125/taller/ej3/Test/baldosasDelPisoTEST.cpp
+++ Laboratorios Algoritmos I/Laboratorio 06-20200125/taller/ej3/Test/baldosasDelPisoTEST.cpp
@@ -2,3 +2,30 @@
 #include "gtest/gtest.h"
 
 // Escribir tests aca:
+
+TEST(ambosDivisibles,baldosas){
+    int N=4;
+    int M=4;
+    int B=2;
+    EXPECT_EQ(baldosasDelPiso(M,N,B),4);
+}
+
+TEST(mDivisible,baldosas){
+    int N=6;
+    int M=12;
+    int B=4;
+    EXPECT_EQ(baldosasDelPiso(M,N,B),6);
+}
+
+TEST(nDivisible,baldosas){
+    int N=50;
+    int M=12;
+    int B=10;
+    EXPECT_EQ(baldosasDelPiso(M,N,B),10);
+}
+TEST(ningunoDivisible,baldosas){
+    int N=6;
+    int M=6;
+    int B=4;
+    EXPECT_EQ(baldosasDelPiso(M,N,B),4);
+}
--- Laboratorios Algoritmos I/Laboratorio 06-20200125/taller/ej3/baldosasDelPiso.cpp
+++ Laboratorios Algoritmos I/Laboratorio 06-20200125/taller/ej3/baldosasDelPiso.cpp
@@ -1,4 +1,30 @@
 int baldosasDelPiso(int M, int N, int B) {
-	return 0;
-	// Borrar el return dummy y completar
+    /*el problema original dice que los lados de las baldosas deben estar paralelos
+     * al borde del suelo.*/
+
+    int anchoOcupado = 0;
+    int altoOcupado = 0;
+    int nroHorizontales = 0;
+    int nroVerticales = 0;
+
+    while (anchoOcupado!=M) {
+        if (anchoOcupado + B >= M) {
+            anchoOcupado = M;
+            nroHorizontales++;
+        } else {
+            anchoOcupado+=B;
+            nroHorizontales++;
+        }
+    }
+    while (altoOcupado!=N) {
+        if (altoOcupado + B >= N) {
+            altoOcupado = N;
+            nroVerticales++;
+        } else {
+            altoOcupado+=B;
+            nroVerticales++;
+        }
+    }
+    return nroVerticales*nroHorizontales;
+    // Borrar el return dummy y completar
 }
