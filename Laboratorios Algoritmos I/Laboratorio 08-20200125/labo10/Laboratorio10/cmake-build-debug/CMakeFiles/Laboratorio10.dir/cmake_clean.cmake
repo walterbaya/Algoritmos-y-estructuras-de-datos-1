983e520e6e10000482d4a869293d42140ac09e63
walterbaya <walterbaya@yahoo.com>
2021-01-06 14:28:47 -0300

    se sube ejercicio 1

---
+++ Laboratorios Algoritmos I/Laboratorio 08-20200125/labo10/Laboratorio10/cmake-build-debug/CMakeFiles/Laboratorio10.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Laboratorio10.dir/src/ejercicios.cpp.o"
+  "CMakeFiles/Laboratorio10.dir/src/risas.cpp.o"
+  "CMakeFiles/Laboratorio10.dir/tests/risasTEST.cpp.o"
+  "CMakeFiles/Laboratorio10.dir/tests/tests.cpp.o"
+  "Laboratorio10"
+  "Laboratorio10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Laboratorio10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Laboratorios Algoritmos I/Laboratorio 08-20200125/labo10/Laboratorio10/src/ejercicios.cpp
@@ -0,0 +1,64 @@
+#include "ejercicios.h"
+
+/* Para compilar */ 
+
+using namespace std;
+
+vector<string> split(string s, char delim){
+	vector<string> v;
+	string palabra;
+    for (int i = 0; i <= s.size(); i++) {
+        if(s[i]==delim){
+            if(palabra.size()>0) {
+                v.push_back(palabra);
+                palabra = "";
+            }
+        }
+        else{
+            palabra+=s[i];
+            if(i == s.size()-1){
+                v.push_back(palabra);
+            }
+        }
+    }
+    return v;
+}
+
+
+string darVueltaPalabra(string s){
+	string res;
+	return res;
+}
+
+// Decidir si s1 es substring o subsecuencia de s2
+bool subsecuencia(string s1, string s2) {
+	int cont = 0;
+	return cont == 0;
+}
+
+// Mismo orden relativo de conjunto de anagramas y dentro de cada vector y con repetidos
+vector<vector<string> > agruparAnagramas(vector<string> v){
+	vector<vector<string> > res;
+	return res;
+}
+
+bool esPalindromo(string s){
+	bool cumple = true;
+	return cumple;
+}
+
+bool tieneRepetidos(string s){
+	bool distinto = true;
+	return !distinto;
+}
+
+string rotar(string s, int j){
+	string res(s);
+	return res;
+}
+
+// no se puede usar substring
+string darVueltaK(string s, int k){
+	string res(s);
+	return res;
+}
